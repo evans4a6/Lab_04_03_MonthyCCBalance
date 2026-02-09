f7d372a1b27c280952cf8fe08789104bef02be7c
addisonevans <evans4a6@mail.uc.edu>
2026-02-08 19:48:58 -0500

    Lab 04, Task 03 complete

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
-final double INTEREST_RATE = 0.17;
-double balance = 5000.0;
-double monthlyRate = 0.0;
-double oneBalance = 0.0;
-double twoBalance = 0.0;
+    final double INTEREST_RATE = 0.17;
+    double balance = 5000.0;
+    double monthlyRate = 0.0;
+    double oneBalance = 0.0;
+    double twoBalance = 0.0;
 
-monthlyRate = balance * INTEREST_RATE;
-oneBalance = monthlyRate + balance;
-twoBalance = (oneBalance * INTEREST_RATE) + oneBalance;
-        System.out.println("Your credit card balance is " + balance + " dollars. After one month of interest, the new balance is " + oneBalance + " dollars.");
-        System.out.println("Your credit card balance after two months is " + twoBalance + " dollars.");
+    monthlyRate = balance * INTEREST_RATE;
+    oneBalance = monthlyRate + balance;
+    twoBalance = (oneBalance * INTEREST_RATE) + oneBalance;
+    System.out.println("Your credit card balance is " + balance + " dollars. After one month of interest, the new balance is " + oneBalance + " dollars.");
+    System.out.println("Your credit card balance after two months is " + twoBalance + " dollars.");
     }
 }
